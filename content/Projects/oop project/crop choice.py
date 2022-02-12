b4dbfbdb2dc1d9b44f4daed8c41acb015d4995de
Hanif-Musaheb <90515435+Hanif-Musaheb@users.noreply.github.com>
2022-02-12 20:53:27 +0000

    Update crop choice.py

--- content/Projects/oop project/crop choice.py
+++ content/Projects/oop project/crop choice.py
@@ -1,8 +1,10 @@
 from wheat import *
-from potatoe import *
+from potato import *
+from cow import*
+from sheep import*
 
 def display_menu():
-    print('which crop would you like to create\n1. Potato\n2.wheat')
+    pass
 
 def select_option():
     valid_option=False
@@ -17,7 +19,37 @@ def select_option():
     return choice
 
 def create_crop():
-    display_menu()
-    if choice==1:
-        new
+    print('which crop would you like to create\n1. Potato\n2. Wheat')
+    choice = select_option()
+    if choice==1:new_crop=potato()
+    elif choice==2: new_crop=wheat()
+    return new_crop
+
+def create_animal(animal_name):
+    print('which animal would you like to chose\n1. Cow\n2. Sheep')
+    choice =select_option()
+    if choice == 1:
+        new_animal=cow(animal_name)
+    elif choice==2:
+        new_animal=sheep(animal_name)
+    return new_animal
+
+animal_exists=False
+def main():
+    global animal_exists
     
+    new_crop = create_crop()
+    manage_crop(new_crop)
+    
+    choice = input('Would you like to add an animal? (y/n)')
+    if choice == 'y':
+        animal_name=str(input('The animals name'))
+        new_animal=create_animal(animal_name)
+        animal_exists==True
+        manage_animal(new_animal)
+        
+    if animal_exists==True:
+        manage_animal(new_animal)
+
+if __name__ == '__main__':
+    main()
